c3bcad1b4ec13b14b7602977e204b2fd7c782e5f
anxxt <antonio.garcia-garcia@outlook.com>
2021-02-20 14:58:03 +0100

    home hecho

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    new TypeIt("#nameTypeIt", {
+        strings: ["Hi, I'm Antonio García.", "I am Enthusiastic.", "I Love Develop & Design."],
+        speed: 125,
+        breakLines: false,
+        waitUntilVisible: true
+      }).go();
+});
