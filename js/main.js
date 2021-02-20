ead1221631f718bae31a483ebeb9f7cabba06069
anxxt <antonio.garcia-garcia@outlook.com>
2021-02-20 21:30:06 +0100

    acabado en principio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
     new TypeIt("#nameTypeIt", {
         strings: ["Hi, I'm Antonio García.", "I am Enthusiastic.", "I Love Develop & Design."],
-        speed: 125,
+        speed: 100,
         breakLines: false,
         waitUntilVisible: true
       }).go();
