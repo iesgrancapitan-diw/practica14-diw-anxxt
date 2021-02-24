1b5fb872c631476216aea478dac3f0561e2526d0
anxxt <antonio.garcia-garcia@outlook.com>
2021-02-24 19:51:53 +0100

    añadido menu hamburguer y cambiado el nav en pc

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 $(function() {
     new TypeIt("#nameTypeIt", {
-        strings: ["Hi, I'm Antonio García.", "I am Enthusiastic.", "I Love Develop & Design."],
-        speed: 100,
-        breakLines: false,
-        waitUntilVisible: true
-      }).go();
+      strings: ["Hi, I'm Antonio García.", "I am Enthusiastic.", "I Love Develop & Design."],
+      speed: 100,
+      breakLines: false,
+      waitUntilVisible: true
+    }).go();
+    let menuImg = $("#menu");
+    let nav = $("nav")
+    menuImg.click(function() {
+      if (menuImg.attr("src") == "icon/menu.svg") {
+        menuImg.attr("src", "icon/close.svg");
+        nav.show("fast");
+      } else {
+        menuImg.attr("src", "icon/menu.svg");
+        nav.hide("fast");
+      }
+    });
 });
